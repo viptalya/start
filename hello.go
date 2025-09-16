a45d400a2f62a52f238dc231090e2f73122c3e16
viptalya <vip_10@bk.ru>
2025-09-16 09:33:16 +0300

    Псевдонимы

--- hello.go
+++ hello.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	Demo8()
+	Demo9()
 }
 
 //Объявление переменных
@@ -620,4 +620,23 @@ func Demo8() {
 	fmt.Println(*p1)
 	p2 := CreatePointer(15)
 	fmt.Println(*p2)
+}
+
+//Псевдонимы
+//Оператор type позволяет определять для типа псевдоним, при этом это не новый тип, а новое название для существующего
+
+//Для типа int определяется псидвоним mile, который основывается на типе int. По сути - это новый тип
+type mile int
+type km int
+
+func Distance(d mile) {
+	fmt.Println(d)
+}
+
+func Demo9() {
+	var d mile = 5
+	Distance(d)
+
+	//var k km = 6
+	//Distance(k) ошибка, т.к. k - это тип km, а наш метод принимает тип mile
 }
